a7b08a02324aa4e9a6d6e331250dd946ffa89160
Vincent <groner04@gmail.com>
2021-08-30 10:48:54 +0200

    second

--- src/backend/insertNew.php
+++ src/backend/insertNew.php
@@ -6,6 +6,7 @@ $email = $_POST["email"];
 $ahv = $_POST["ahv"];
 $personalNr = $_POST["personalNr"];
 $telefonenr = $_POST["telefonenr"];
+$companyName = $_POST["companyName"];
 $department = $_POST["department"];
 $jobTitle = $_POST["jobTitle"];
 $description = $_POST["description"];
